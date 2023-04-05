f760c457fac7f1768067271dfa63e8836ed2aef9
Arda Yildirim <ardayildirim05@icloud.com>
2023-04-05 07:30:25 +0200

    Implement foundation for mobile design

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -4,8 +4,8 @@ export default function Hero() {
   return (
     <div className="hero">
       <div className="hero__image"></div>
-      <div>
-        <p>
+      <div className="hero__content">
+        <p className="hero__content__text">
           Blogify is a service to make your bloging dreams possible. With
           Blogify you can easily create your own bloging studio and create your
           own blogposts. Design your posts, insert and edit images, and make
